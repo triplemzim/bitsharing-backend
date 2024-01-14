51f1103f73484c8189bad075274dd0f883fc140e
triplemzim <triplemzim@gmail.com>
2024-01-15 02:29:35 +0800

    host update

--- bitsharing/settings.py
+++ bitsharing/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-+5@_ld^(k=zv6ih4((9pl*mp)i=r)ggl5gya^vo5$inuh9#t%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bitsharing.vercel.app', 'bitsharing.pythonanywhere.com']
 
 
 # Application definition
